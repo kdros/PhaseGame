511302f4c527f6dbd4f73c0365017533cc7c78fd
Rohith Chandran <rohithX@gmail.com>
2013-07-14 12:11:27 -0400

    Icicle/IcyCeiling script done.

--- Phase/Assets/Scripts/Trap Scripts/Icicle.cs
+++ Phase/Assets/Scripts/Trap Scripts/Icicle.cs
@@ -1,15 +1,33 @@
 using UnityEngine;
 using System.Collections;
 
-public class Icicle : MonoBehaviour {
-
+public class Icicle : MonoBehaviour 
+{
+	public enum IcicleType { Cone, Cube, Cylinder}
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public void detachIcicle (IcicleType iceType)
+	{
+		Transform childObj = gameObject.transform;
+		switch (iceType)
+		{
+		case IcicleType.Cone:
+			childObj = childObj.Find ("pCone1");
+			break;
+		case IcicleType.Cube:
+			childObj = childObj.Find ("pCube1");
+			break;
+		case IcicleType.Cylinder:
+			childObj = childObj.Find ("pCylinder1");
+			break;
+		}
+		
+		childObj.parent = null;
+		childObj.rigidbody.useGravity = true;
+		childObj.rigidbody.AddForce (Vector3.down * 10f);
 	}
 }
--- Phase/Assets/Scripts/Trap Scripts/IcicleParticle.cs
+++ Phase/Assets/Scripts/Trap Scripts/IcicleParticle.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class WindTunnelScript : MonoBehaviour {
+public class IcicleParticle : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +10,6 @@ public class WindTunnelScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.Rotate(0.0f, 0.0f, 10.0f);
+	
 	}
 }

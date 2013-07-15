ff471203788b94e5b4837f54a418744773ba77c1
Kristen Drosinos <kdrosinos713@gmail.com>
2013-07-15 09:49:57 -0400

    MatterScript with empty virtual collision resolution functions

---
+++ Phase/Assets/Scripts/Character Scripts/MatterScript.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+
+public class MatterScript : MonoBehaviour {
+	
+	public virtual void CheckpointCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void FallingBouldersCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void FlamePillarCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void GrateCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void IceCeilingCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void IcyFloorCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void LavaCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void PitfallCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void SpikeCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void SwingingMaceCollisionResolution()
+	{
+		
+	}
+	
+	public virtual void WindTunnelCollisionResolution()
+	{
+		
+	}
+}

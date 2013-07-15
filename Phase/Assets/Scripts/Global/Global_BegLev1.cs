54c0d86f4e1554b04dd6cf17b9dcb90aeea4f307
Kristen Drosinos <kdrosinos713@gmail.com>
2013-07-14 21:03:35 -0400

    Beginner Level 1 - Checkpoint Testing/Setup

--- Phase/Assets/Scripts/Global/Global_BegLev1.cs
+++ Phase/Assets/Scripts/Global/Global_BegLev1.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Global_BegLev1 : MonoBehaviour {
 	
 	public int numCheckpoints;
-	public int checkpointHit;
+	public int hitCheckpoints;
 	public Vector3[] checkpointPos;
 	public GameObject mattySolid;
 	public bool mattySolidDeath;
@@ -12,14 +12,15 @@ public class Global_BegLev1 : MonoBehaviour {
 	void Start ()
 	{
 		// Set number of checkpoints that are in the scene
-		numCheckpoints = 1;
+		numCheckpoints = 2;
 		
 		// Since there is a checkpoint at the beginning of each level, set to 1
-		checkpointHit = 1;
+		hitCheckpoints = 1;
 		
 		// Set checkpoint positions here
-		checkpointPos = new Vector3[1]; 
+		checkpointPos = new Vector3[2]; 
 		checkpointPos[0] = new Vector3(-8.523637f, 2.04423f, 3.322249f);
+		checkpointPos[1] = new Vector3(-4.563113f, 12.23711f, 3.322249f);
 		
 		mattySolidDeath = false;
 	}
@@ -33,7 +34,7 @@ public class Global_BegLev1 : MonoBehaviour {
 		{
 			Debug.Log ("Matty Solid Character was destroyed! Restoring...");
 			// Need to restor character to last checkpoint
-			Vector3 restorePos = checkpointPos[0];
+			Vector3 restorePos = checkpointPos[(hitCheckpoints - 1)];
 			Instantiate(mattySolid, restorePos, Quaternion.identity );
 			
 			// Reset boolean variable

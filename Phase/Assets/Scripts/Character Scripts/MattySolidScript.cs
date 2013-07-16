ec5550611c5c48a5f85aa8c426d031e5f49b6ced
Kristen Drosinos <kdrosinos713@gmail.com>
2013-07-16 10:45:57 -0400

    Replaced cube with solid matty character. The movements are still off but the character is in and sized correctly for now! (Height still needs to be fixed for him and default matty)

--- Phase/Assets/Scripts/Character Scripts/MattySolidScript.cs
+++ Phase/Assets/Scripts/Character Scripts/MattySolidScript.cs
@@ -4,7 +4,56 @@ using System.Collections;
 public class MattySolidScript : MatterScript 
 {
 	//public MatterScript matter_solid;
+	public float facingRight_yDegrees;
+	public float facingLeft_yDegrees;
+	
+	void Start ()
+	{
+		//matter_matty = new MatterScript();
+		
+		facingRight_yDegrees = gameObject.transform.eulerAngles.y + 180;
+		facingLeft_yDegrees = gameObject.transform.eulerAngles.y;
+		
+		//gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+										//gameObject.transform.position.y - 100.0f,
+										//gameObject.transform.position.z);
 
+		transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x,
+												facingRight_yDegrees,
+												gameObject.transform.eulerAngles.z);
+		
+		// Set Idle and Walk to loop
+		gameObject.animation["Walk"].wrapMode = WrapMode.Loop;
+		gameObject.animation["Idle"].wrapMode = WrapMode.Loop;
+	   
+		// Set Jump to only play once
+		gameObject.animation["Jump"].wrapMode = WrapMode.Once;
+		
+		gameObject.animation.Play("Idle", PlayMode.StopAll);
+		
+	}
+	
+	void OnGUI ()
+	{
+		if(Input.GetKey("left"))
+		{
+			transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x,
+												facingLeft_yDegrees,
+												gameObject.transform.eulerAngles.z);
+			gameObject.animation.Play("Walk", PlayMode.StopAll);
+		}
+		else if(Input.GetKey("right"))
+		{
+			transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x,
+												facingRight_yDegrees,
+												gameObject.transform.eulerAngles.z);
+			gameObject.animation.Play("Walk", PlayMode.StopAll);
+		}
+		else if(Input.GetKey("space"))
+			gameObject.animation.Play("Jump", PlayMode.StopAll);
+		else
+			gameObject.animation.Play("Idle", PlayMode.StopAll);
+	}
 	
 	// Functions that do not need to be overriden:
 	// FallingBoulders, FlamePillar, Grate, Lava, Pitfall

19d356eb64c9d9098bb6ba9c79380362f5432a93
Rohith Chandran <rohithX@gmail.com>
2013-07-16 15:16:06 -0400

    Added currentSave to gitignore. Also, trying to make Icicles melt when they hit Lava (still buggy).

--- Phase/Assets/Scripts/Trap Scripts/IcicleParticle.cs
+++ Phase/Assets/Scripts/Trap Scripts/IcicleParticle.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class IcicleParticle : MonoBehaviour 
 {
+	float curLife;
 	// Use this for initialization
 	void Start () 
 	{
-	
+		curLife = 1.0f;
 	}
 	
 	void OnCollisionEnter (Collision collObj) 
@@ -14,7 +15,17 @@ public class IcicleParticle : MonoBehaviour
 		Collider collObjCollider = collObj.collider;
 		if (collObjCollider.CompareTag ("Ground") || collObjCollider.CompareTag ("Platform"))
 			rigidbody.useGravity = false;
-		
+		else if (collObjCollider.CompareTag ("Lava"))
+		{
+			if (curLife > 0.1f)
+			{
+				curLife -= 0.1f;
+				float originalY = transform.position.y;
+				transform.localScale *= curLife;
+				transform.position = new Vector3 (transform.position.x, originalY, transform.position.z);
+			}
+			
+		}
 //		Debug.Log (collObjCollider.gameObject.name);
 //		Debug.Log (collObjCollider.gameObject.tag);
 	}

b4bbf0dd09d29b761d3c5a7285b1c2dc72bb9b51
Kristen Drosinos <kdrosinos713@gmail.com>
2013-07-31 22:48:16 -0400

    Added intermediate level buttons to game menu page. (So levels are easier to access for beta demo)

--- Phase/Assets/Scripts/Global/GameMenuScript.cs
+++ Phase/Assets/Scripts/Global/GameMenuScript.cs
@@ -19,11 +19,11 @@ public class GameMenuScript : MonoBehaviour {
 		
 		//Load the main scene
 		//The scene needs to be added into build setting to be loaded!
-		if(GUILayout.Button("Instructions"))
-		{
-			Debug.Log ("No Instructions Yet!");
+		//if(GUILayout.Button("Instructions"))
+		//{
+			//Debug.Log ("No Instructions Yet!");
 			//Application.LoadLevel("");
-		}
+		//}
 		if(GUILayout.Button("Let's Play!"))
 		{
 			Application.LoadLevel(2);
@@ -43,6 +43,18 @@ public class GameMenuScript : MonoBehaviour {
 		if(GUILayout.Button("Beginner Level 4"))
 		{
 			Application.LoadLevel(5);
+		}
+		if(GUILayout.Button("Intermediate Level 1"))
+		{
+			Application.LoadLevel(6);
+		}
+		if(GUILayout.Button("Intermediate Level 2"))
+		{
+			Application.LoadLevel(7);
+		}
+		if(GUILayout.Button("Intermediate Level 3"))
+		{
+			Application.LoadLevel(8);
 		}
 		if(GUILayout.Button("Exit"))
 		{

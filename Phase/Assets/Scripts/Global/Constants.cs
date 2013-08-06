be5d08db6168deed42d11377daad049225baf0d5
Mikey Chen <jajankenn@gmail.com>
2013-08-06 12:03:54 -0400

    merged

--- Phase/Assets/Scripts/Global/Constants.cs
+++ Phase/Assets/Scripts/Global/Constants.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Constants : MonoBehaviour 
 {
 	public static int introSceneIndex = 2;								// index of the introductory scene (first playable level)
+	public static double defaultSpeedRecord = 362439;					// speed run records are initialized to this value
 	public static string LevelToLoadKey = "LevelToLoad";				// key for playerpref used for choosing which scene to load
 	public static string GameModeKey = "GameMode";						// key for playerpref used for switching game modes
 	public static string GameModeNormal = "Normal";						// value for playerpref's GameMode key that represents normal game mode
@@ -17,4 +18,5 @@ public class Constants : MonoBehaviour
 	public static string level5SpeedRunDataKey = "lev5SpeedRun";		// key for playerpref used for keeping track of level 5 speed run data
 	public static string level6SpeedRunDataKey = "lev6SpeedRun";		// key for playerpref used for keeping track of level 6 speed run data
 	public static string level7SpeedRunDataKey = "lev7SpeedRun";		// key for playerpref used for keeping track of level 7 speed run data
+	public static string SpeedRunFilePath = "Save/Record";				// path to the file that speed run records are being stored
 }
--- Phase/Assets/Scripts/Global/RecordManager.cs
+++ Phase/Assets/Scripts/Global/RecordManager.cs
@@ -1,20 +1,44 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 // Utility class for managing the speed run records
 // Stores a file in Save/records
 public class RecordManager : MonoBehaviour {
 	
-	List<Dictionary<string,double>> allRecords;	 
+	// Parses file for the record of all playable levels.
+	// Each level will have maximum of 5 records
+	List<Dictionary<string,double>> allRecords;	  
 	
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		if (System.IO.File.Exists (Constants.SpeedRunFilePath))
+		{
+			
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	private void readRecords()
+	{
+			
+	}
+	
+	// Add the record to allRecords
+	public void addRecord(int sceneIndex, double recondInSeconds)
+	{
+			
+	}
+	
+	// write allRecords to Save/Records
+	public void saveRecords()
+	{
+		
 	}
 }

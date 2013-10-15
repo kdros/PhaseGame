318bf4c84407530ca9bca02fed101db4334cc373
Mikey Chen <jajankenn@gmail.com>
2013-10-15 14:57:27 -0400

    commented boulder script

--- Phase/Assets/Scripts/Trap Scripts/Boulder.cs
+++ Phase/Assets/Scripts/Trap Scripts/Boulder.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
+// Boulder class to be attached to the boulder prefab. Boulder is responsible for doing
+// collision against the death plane.
 public class Boulder : MonoBehaviour 
 {	
+	// Collision detection and resolution.
 	void OnCollisionEnter (Collision collision)
 	{
 		Collider collider = collision.collider;

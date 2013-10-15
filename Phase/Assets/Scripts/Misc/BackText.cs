c7513eb7047362208eb36d0c17c90e4b1d05e56d
Mikey Chen <jajankenn@gmail.com>
2013-10-15 15:26:07 -0400

    Commented backText and textscript

--- Phase/Assets/Scripts/Misc/BackText.cs
+++ Phase/Assets/Scripts/Misc/BackText.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using System.Collections;
 
+// BackText is a inherits TextScript and is responsible for defining the behavior of the back text in the game menu
 public class BackText : TextScript 
 {
+	// Go back to the main menu if the back text is clicked
 	public override void respondToClick ()
 	{
 		if (this.enabled)
 		{
-			Debug.Log("need to switch to mode menu");
+			//Debug.Log("need to switch to mode menu");
 			GameObject gm = GameObject.FindGameObjectWithTag ("GameMenu");
 			GameMenu gameMenu = gm.GetComponent<GameMenu>();
 			gameMenu.toMainMenu();
